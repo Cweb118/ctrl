61590e85ec1ed28299f4861f75e85f0069386cb4
James <jamsspinle1@gmail.com>
2022-01-09 03:50:09 -0500

    added the merchant and the traveler

--- _00_cogs/sudo.py
+++ _00_cogs/sudo.py
@@ -1,20 +1,22 @@
+import time
 import nextcord
 from nextcord import slash_command
 from nextcord import webhook
 from nextcord.ext import commands
 
 guilds = [588095612436742173, 778448646642728991]
-
+profiles = {"merchant" : {"name" : "The Merchant", "pfp" : "https://media.discordapp.net/attachments/825443696152543314/929653537682632725/frog_final_2.jpg?width=810&height=671"},
+            "traveler" : {"name" : "The Traveler", "pfp" : "https://media.discordapp.net/attachments/160848693044445184/929636685359222834/unknown.jpg?width=528&height=669"}}
 class Sudo(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
     
     @slash_command(name="sudo", guild_ids=guilds)
-    async def sudo(self, ctx, channel, message):
+    async def sudo(self, ctx, profile, channel, message):
         channel = nextcord.utils.get(ctx.guild.text_channels, name=channel)
-        webhook = await channel.create_webhook(name="Test")
-        await webhook.send(message, username="test", avatar_url="https://cdn.discordapp.com/attachments/825443696152543314/929510309239062568/Untitled-2.png")
-
+        webhook = await channel.create_webhook(name=time.time())
+        await webhook.send(message, username=profiles[profile]["name"], avatar_url=profiles[profile]["pfp"])
+        await webhook.delete()
 
 def setup(bot):
     bot.add_cog(Sudo(bot))
