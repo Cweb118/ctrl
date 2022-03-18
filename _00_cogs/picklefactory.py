71f66c8d619decd201ef9c553387b894804780a9
James <jamsspinle1@gmail.com>
2022-03-18 18:48:22 -0400

    idk

--- _00_cogs/picklefactory.py
+++ _00_cogs/picklefactory.py
@@ -9,8 +9,8 @@ from nextcord.ext import commands
 """
 IMPORT ALL VARIABLES TO SAVE HERE.
 """
-from _02_global_dicts import district_dict, region_dict, resource_dict, player_dict
-varsToSave = {"player_dict" : player_dict, "resource_dict" : resource_dict, "region_dict" : region_dict, "district_dict" : district_dict}
+from _02_global_dicts import *
+varsToSave = {"player_dict" : player_dict, "resource_dict" : resource_dict, "region_dict" : region_dict, "district_dict" : district_dict, "played_cards_dict" : played_cards_dict}
 """
 ALSO ADD ALL VARIABLSE TO varsToSave LIST
 """
@@ -31,75 +31,53 @@ class PickleFactory(commands.Cog):
             with open(f"{os.getcwd()}\\_01_pickle_jar\\{dictKey}.pkl", "wb") as file:
                 pickle.dump(varsToSave[dictKey], file)
 
-        #with open(f"{os.getcwd()}\\_01_pickle_jar\\currentSave.pkl", "wb") as file:
-        #    pickle.dump(varsToSave, file)
-        
         await ctx.send("Save Created!")
     
     @commands.command(name="load")
     async def loadAll(self, ctx):
-            player_dict.clear()
-            resource_dict.clear()
-            region_dict.clear()
-            district_dict.clear()
+        global played_cards_dict
 
-            with open(f"{os.getcwd()}\\_01_pickle_jar\\resource_dict.pkl", "rb") as file:
-                resources = pickle.load(file)
-                for key in resources:
-                    resource_dict[key] = resources[key]
+        player_dict.clear()
+        resource_dict.clear()
+        region_dict.clear()
+        district_dict.clear()
+        played_cards_dict.clear()
 
-            with open(f"{os.getcwd()}\\_01_pickle_jar\\player_dict.pkl", "rb") as file:
-                players = pickle.load(file)
-                for key in players:
-                    player_dict[key] = players[key]
-                    player_dict[key].reinstate(self.bot)
-            
-            with open(f"{os.getcwd()}\\_01_pickle_jar\\region_dict.pkl", "rb") as file:
-                regions = pickle.load(file)
-                for key in regions:
-                    region_dict[key] = regions[key]
+        with open(f"{os.getcwd()}\\_01_pickle_jar\\resource_dict.pkl", "rb") as file:
+            resources = pickle.load(file)
+            for key in resources:
+                resource_dict[key] = resources[key]
 
-            #reinstating regions
-            for regionKey in region_dict.keys():
-                region_dict[regionKey].reinstate(self.bot.get_guild(region_dict[regionKey].guildID))
-                for district in region_dict[regionKey].districts:
-                    district_dict[district.name] = district
-
-            with open(f"{os.getcwd()}\\_01_pickle_jar\\district_dict.pkl", "rb") as file:
-                districts = pickle.load(file)
-                for key in districts:
-                    district_dict[key] = districts[key]
-
-            for key in district_dict.keys():
-                district_dict[key].reinstate(self.bot.get_guild(district_dict[key].guildID))
-
-
-
-            
-            """
-            #load resources
-            for key in varsToLoad[1].keys():
-                print(key, varsToLoad[1][key])
-                resource_dict[key] = varsToLoad[1][key]
-
-            #load players
-            for key in varsToLoad[0].keys():
-                player_dict[key] = varsToLoad[0][key]
+        with open(f"{os.getcwd()}\\_01_pickle_jar\\player_dict.pkl", "rb") as file:
+            players = pickle.load(file)
+            for key in players:
+                player_dict[key] = players[key]
                 player_dict[key].reinstate(self.bot)
-            
-            #load regions
-            for key in varsToLoad[2].keys():
-                region_dict[key] = varsToLoad[2][key]
-                await region_dict[key].reinstate(self.bot)
-            
-            #Load districts
-            for regionKey in region_dict.keys():
-                print("region:", regionKey)
-                for district in region_dict[regionKey].districts:
-                    print("district:", district)
-                    district_dict[district.name] = district
-            """
-            await ctx.send("Save Loaded!")
+        
+        with open(f"{os.getcwd()}\\_01_pickle_jar\\region_dict.pkl", "rb") as file:
+            regions = pickle.load(file)
+            for key in regions:
+                region_dict[key] = regions[key]
+
+        #reinstating regions
+        for regionKey in region_dict.keys():
+            region_dict[regionKey].reinstate(self.bot.get_guild(region_dict[regionKey].guildID))
+            for district in region_dict[regionKey].districts:
+                district_dict[district.name] = district
+
+        with open(f"{os.getcwd()}\\_01_pickle_jar\\district_dict.pkl", "rb") as file:
+            districts = pickle.load(file)
+            for key in districts:
+                district_dict[key] = districts[key]
+
+        #reinstating districts
+        for key in district_dict.keys():
+            district_dict[key].reinstate(self.bot.get_guild(district_dict[key].guildID))
+
+        with open(f"{os.getcwd()}\\_01_pickle_jar\\played_cards_dict.pkl", "rb") as file:
+            played_cards_dict = pickle.load(file)
+
+        await ctx.send("Save Loaded!")
 
 def setup(bot):
     bot.add_cog(PickleFactory(bot))
