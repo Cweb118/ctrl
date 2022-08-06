5a25d2aa5a2a5c5f8ac9c40dcb65613ad268f03a
Ginger_Walnut <ginger_walnut@comcast.net>
2022-08-05 20:56:23 -0700

    Trait Actions

--- _00_cogs/architecture/kits/character_kits.py
+++ _00_cogs/architecture/kits/character_kits.py
@@ -4,7 +4,7 @@ from _00_cogs.mechanics.building_classes._building_kits import building_kits_dic
 
 g0_character_kits = {
     #The Cartographer (me)
-    160020690051792898:[160020690051792898, 1, 'Yavar', 'Yavari',
+    161520114657656832:[161520114657656832, 1, 'Yavar', 'Yavari',
      {'resources':{'Food':10,'Water':10},
       'units':[['Yavari','Worker'],['Yavari','Worker']],
       'buildings':[bk['harmonist_scout_guild'],bk['mother_tree']]}],
